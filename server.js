6f6467acb52947c125d78fec164933ef443e9d82
Jae Min Im <jaeminim@berkeley.edu>
2024-03-28 14:09:28 -0700

    basic page navigation set

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require('express');
 var http = require('http');
-var fs = require('fs'); 
+var url = require('url');
+var fs = require('fs');
 const { prototype } = require('module');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Hello server is running')
-    .end();
-});
- 
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .send('Hello server is running')
+//     .end();
+// });
+
 
 http.createServer(function (req, res) {
-  fs.readFile('index.html', function(err, data) {
+  var q = url.parse(req.url, true);
+  
+  var filename = "./static/view/404.html";
+  if (q.pathname=="" || q.pathname=="/") {
+    filename = "./view/index.html";
+  } else {
+
+  }
+  console.log(q.pathname)
+  
+  fs.readFile(filename, function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
