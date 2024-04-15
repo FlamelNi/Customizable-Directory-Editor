e75c55e1eafdb3503fd6986ce34a31011a5b7891
Jae Min Im <jaeminim@berkeley.edu>
2024-04-15 13:12:30 -0700

    basic bootstrap ui refined with buttons and inputs

--- js/index.js
+++ js/index.js
@@ -5,5 +5,35 @@ function testFunction() {
     // new zip.BlobReader(file);
 }
 
+directory_data = {
+    column_row: [],
+
+    /*
+    rows: [
+        ['', ''],
+        ['', ''],
+    ],
+    */
+   rows: [],
+    
+}
+
+function get_HTML_column() {
+
+}
+
+function get_HTML_column_row() {
+
+}
+
+function get_HTML_row() {
+
+}
+
+function get_HTML_all_rows() {
+
+}
+
+
 
 
